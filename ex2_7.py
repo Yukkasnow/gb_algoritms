7ab419cb61e472ff356b59272fd5b21741f7bd71
YuliaE <yulia2000.egorova@yandex.ru>
2022-04-19 21:47:12 +0300

    ex7

---
+++ ex2_7.py
@@ -0,0 +1,14 @@
+n=int(input('Введите n'))
+a=1
+summ=1
+while a!=n:
+    summ=summ+(a+1)
+    print(summ, a)
+    a += 1
+
+b=n*(n+1)/2
+
+if summ==b:
+    print('Равенство выполняется')
+else:
+    print('Равенство не выполняется')
