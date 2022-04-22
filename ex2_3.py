6eaadcbf41230eb2cf76795254266d49f4ed310b
YuliaE <yulia2000.egorova@yandex.ru>
2022-04-22 15:22:44 +0300

    .

--- ex2_3.py
+++
@@ -1,3 +0,0 @@
-num=input('Введите число')
-result=num[::-1]
-print(result)
--- ex2_8.py
+++
@@ -1,7 +0,0 @@
-number_range=input('Введите поседовательность цифр ')
-find_number=input('Ввведите цифру количество которой вы хотете посчитать ')
-i=0
-for num in number_range:
-    if num==find_number:
-        i+=1
-print(f'{find_number} встречается {i} раз')
