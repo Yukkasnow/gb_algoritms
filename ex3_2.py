192bdba21a11de25ca9a7fdbf6daf25cdb95f631
YuliaE <yulia2000.egorova@yandex.ru>
2022-05-02 16:55:09 +0300

    ex1

--- ex3_2.py
+++
@@ -1,6 +0,0 @@
-mass_1=[10,25,30,45,50]
-mass_2=[]
-for i in mass_1:
-    if i%2==0:
-        mass_2.append(mass_1.index(i))
-print(mass_2)
--- ex3_4.py
+++
@@ -1,9 +0,0 @@
-mass=[1,2,8,8,6,7,7,9,3,3,4,8,5,1,8,6,8]
-print(mass)
-total=0
-for m in mass:
-    count=mass.count(m)
-    if count>total:
-        total=count
-        max_number=m
-print(f'{max_number} встречается {total} раз')
